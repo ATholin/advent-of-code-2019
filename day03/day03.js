5f6e00cb22203f682906b8907d151458880e7220
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2019-12-03 14:30:15 +0100

    day03 cleanup (still slow)

--- day03/day03.js
+++ day03/day03.js
@@ -8,7 +8,7 @@ const calcPath = (path, index) => {
     path.split(',').forEach(step => {
         const dir = step[0]
         const c = step.slice(1)
-        
+
         switch(dir) {
             case 'U':
                 for (let i = 0; i < c; i++) {
@@ -50,28 +50,36 @@ const calcPath = (path, index) => {
 
 const calcDistance = pos => Math.abs(pos.x) + Math.abs(pos.y)
 
-const data = fs.readFileSync('day03_test.in', 'utf8');
+const data = fs.readFileSync('day03.in', 'utf8');
 
 const calculatedPaths = data.split('\n').map(calcPath)
 
 const path1 = calculatedPaths[0]
 const path2 = calculatedPaths[1]
+const len = path1.length
+
+console.log('Parsing complete\n')
 
 let fewestSteps = 99999
 let closest = 99999
-//part2
+
 path1.forEach((path, i) => {
-    if (path2.filter(p => p.x == path.x && p.y == path.y).length > 0 && path.x != 0 && path.y != 0) {
+    if (path.x == 0 && path.y == 0) return
+    if (path2.some(val => val.x == path.x && val.y == path.y)) {
         const distance = calcDistance(path);
         closest = Math.min(closest, distance)
-        
+
         const steps1 = i
         const steps2 = path2
             .findIndex(p => p.x == path.x && p.y == path.y)
         const steps = steps1+steps2
 
+        console.log(`(${i}/${len})Distance: ${distance}, steps: ${steps}`)
+
         fewestSteps = Math.min(fewestSteps, steps)
     }
 })
-console.log("(PART1) Closest steps:", closest)
-console.log("(PART2) Fewest steps:", fewestSteps)
+
+console.log('\nDONE\n')
+console.log("[PART1] Closest steps:", closest)
+console.log("[PART2] Fewest steps:", fewestSteps)
