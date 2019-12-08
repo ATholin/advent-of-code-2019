354091481fecbff64b554338a035f31d6f75dcae
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2019-12-08 17:35:30 +0100

    Day05 uses lib/intcode, day02p1 bad code golf

--- day02/day02.js
+++ day02/day02.js
@@ -1,19 +1,15 @@
 const { IntCode } = require('../lib/intcode')
 
-const part1 = (input, p1, p2) => {
-
-
-    const ic = new IntCode([...input], "ICP1")
-    ic.setAtPosition(1, p1)
-    ic.setAtPosition(2, p2)
-
-    return ic.execute().getAtPosition(0)
-}
+const part1 = (input, p1, p2) => new IntCode([...input])
+                                        .setAtPosition(1, p1)
+                                        .setAtPosition(2, p2)
+                                        .execute()
+                                        .getAtPosition(0)
 
 const part2 = input => {
     for (let i = 0; i < 100; i++) {
         for (let j = 0; j < 100; j++) {
-            const ic = new IntCode([...input], "ICP1")
+            const ic = new IntCode([...input])
             ic.setAtPosition(1, i).setAtPosition(2, j)
 
             if (ic.execute().getAtPosition(0) == 19690720) {
