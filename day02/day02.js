3c9bba485611c1772053172d307dee8cca2986a2
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2019-12-08 17:25:07 +0100

    Day02 uses lib/intcode

--- day02/day02.js
+++ day02/day02.js
@@ -1,34 +1,31 @@
-const parseInstr = (instr, rs, rd) => {
-    if (instr == 99) return -1
-    return instr == 1 ? rs + rd : rs * rd
+const { IntCode } = require('../lib/intcode')
+
+const part1 = (input, p1, p2) => {
+
+
+    const ic = new IntCode([...input], "ICP1")
+    ic.setAtPosition(1, p1)
+    ic.setAtPosition(2, p2)
+
+    return ic.execute().getAtPosition(0)
 }
 
-module.exports.part1 = input => {
-    let cnt = 0
-    while (cnt < input.length) {
-        const instr = input[cnt]
-        if (instr == 99) break
-        const rs = input[input[cnt + 1]]
-        const rd = input[input[cnt + 2]]
-        const o = input[cnt + 3]
-        const res = parseInstr(instr, rs, rd)
-        input[o] = res
-        cnt += 4
-    }
-
-    return input
-}
-
-module.exports.part2 = input => {
+const part2 = input => {
     for (let i = 0; i < 100; i++) {
         for (let j = 0; j < 100; j++) {
-            input[1] = i
-            input[2] = j
-    
-            if (this.part1([...input])[0] == 19690720) {
-                console.log("FOUND!", input[1], input[2])
-                return (100 * input[1]) + input[2]
+            const ic = new IntCode([...input], "ICP1")
+            ic.setAtPosition(1, i).setAtPosition(2, j)
+
+            if (ic.execute().getAtPosition(0) == 19690720) {
+                return (100 * i) + j
             }
         }
     }
+
+    return -1
+}
+
+module.exports = {
+    part1,
+    part2
 }
